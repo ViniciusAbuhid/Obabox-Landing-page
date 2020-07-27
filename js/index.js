e178fe3731cc8500b7535a9176f2f184cf16c66c
Vinicius <viniciusabuhid@gmail.com>
2020-07-27 15:30:30 -0300

    final fixes before delivery

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-$(function(){
-    console.log('hgello world')
-})
+// $.getJSON('../Produto.json', function (data) {
+//     console.log(data.Caracteristicas)
+//     for (let i in data.Caracteristicas) {
+//         if ($(`#${i}`)) {
+//             $(`#${i}`).html(data[i])
+//         }
+//     }
+// })
